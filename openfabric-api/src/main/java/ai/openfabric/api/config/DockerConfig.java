19fa247b07227892193eb730e1431009d0ffd187
aolufisayo <phissycodes@hotmail.com>
2023-05-28 09:57:04 +0100

    setup dockerConfig

---
+++ openfabric-api/src/main/java/ai/openfabric/api/config/DockerConfig.java
@@ -0,0 +1,36 @@
+package ai.openfabric.api.config;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class DockerConfig {
+
+    @Bean
+    public DockerClient dockerClient(){
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+            .withDockerHost("tcp://localhost:2375")
+            .build();
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+            .dockerHost(config.getDockerHost())
+            .sslConfig(config.getSSLConfig())
+            .build();
+
+        DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
+        try {
+            httpClient.close();
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        return dockerClient;
+    }
+}
